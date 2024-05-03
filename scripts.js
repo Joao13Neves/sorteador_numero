2598dec6e2e48122c22e98bc063c7a900899dc80
Joao Neves/ <joaobatista.tec2019@gmail.com>
2024-05-02 23:29:43 -0300

    feature: add function sortear, faltou criar a logica

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 function sortear() {
-    console.log(teste)
+  let quantidade = parseInt(document.getElementById("quantidade").value);
+  let minimo = parseInt(document.getElementById("minimo").value);
+  let maximo = parseInt(document.getElementById("maximo").value);
+
+  let numero = obterNumerosAleatorios(minimo, maximo);
+  alert(numero);
+}
+
+function obterNumerosAleatorios(min, max) {
+  return Math.random() * (maximo - minimo) + minimo;
 }
