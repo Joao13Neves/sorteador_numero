ffb6d89de14ca957a24fa6233e8bcafc4530f55a
Joao13Neves <joaobatista.tec2019@gmail.com>
2024-05-25 00:06:00 -0300

    feat: add result in screen

--- scripts.js
+++ scripts.js
@@ -4,17 +4,29 @@ function sortear() {
   let de = parseInt(document.getElementById("de").value);
   let ate = parseInt(document.getElementById("ate").value);
 
-  //Array para armazenar os números
   let sorteados = [];
   let numero;
 
-  for (let i = 0; i < quantidade, i++;) {
+  for (let i = 0; i < quantidade; i++) {
     numero = obterNumerosAleatorios(de, ate);
+    while (sorteados.includes(numero)) {
+      numero = obterNumerosAleatorios(de, ate);
+    }
     sorteados.push(numero);
   }
+
+  let resultado = document.getElementById("numeros");
+  resultado.innerHTML = `<label class="resultado_numero" id="numeros">${sorteados}</label>`;
+  alterarStatusBotao();
 }
 
 // gerando números aleatórios
 function obterNumerosAleatorios(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function alterarStatusBotao() {
+  let botao = document.getElementById("btn-reiniciar");
+  botao.classList.remove("container__botao");
+  botao.classList.add("container__botao-desabilitado");
 }
